c886ff43783d1055b2f050a0d29f5a24375422c8
Hugo <rodriguez.h@pm.me>
2022-10-19 17:11:17 +0200

    Create Menu and add navbar

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -4,6 +4,15 @@ function Menu() {
   return (
     <div>
       <h1 className="title">LEGENDS</h1>
+      <nav>
+        <ul className="nav">
+          <li>
+            <a href="/">CARTES</a>
+            <a href="/">REGLES</a>
+            <a href="/">INFOS</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
