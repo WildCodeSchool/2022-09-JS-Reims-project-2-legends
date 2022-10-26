979b183dd5e586cdaae057292f51dd971ee26020
Adnan Bakaev <adnanbakaev@MacBook-Air-Adnan.local>
2022-10-26 12:27:41 +0200

    modified card style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,38 +26,40 @@ function App() {
   };
   return (
     <div className="App">
-      <Card
-        image={hero.image}
-        alt={hero.name}
-        name={hero.name}
-        intelligence={hero.intelligence}
-        strength={hero.strength}
-        life={hero.life}
-      />
-      <Card
-        image={enemyInitialState.image}
-        alt={enemyInitialState.name}
-        name={enemyInitialState.name}
-        intelligence={enemyInitialState.intelligence}
-        strength={enemyInitialState.strength}
-        life={enemyInitialState.life}
-      >
-        <div className="flex flex-row w-full">
-          <button
-            type="button"
-            className="bg-red-500 w-full h-8 text-white"
-            onClick={() => {
-              const damage = Math.floor(Math.random() * hero.strength);
-              setEnemyLife(enemy.life - damage);
-            }}
-          >
-            Attack
-          </button>
-          <button type="button" className="bg-blue-500 w-full h-8 text-white">
-            Defend
-          </button>
-        </div>
-      </Card>
+      <div className="flex flex-col gap-36">
+        <Card
+          image={hero.image}
+          alt={hero.name}
+          name={hero.name}
+          intelligence={hero.intelligence}
+          strength={hero.strength}
+          life={hero.life}
+        />
+        <Card
+          image={enemyInitialState.image}
+          alt={enemyInitialState.name}
+          name={enemyInitialState.name}
+          intelligence={enemyInitialState.intelligence}
+          strength={enemyInitialState.strength}
+          life={enemyInitialState.life}
+        >
+          <div className="flex flex-row w-full">
+            <button
+              type="button"
+              className="bg-red-500 w-full h-8 text-white"
+              onClick={() => {
+                const damage = Math.floor(Math.random() * hero.strength);
+                setEnemyLife(enemy.life - damage);
+              }}
+            >
+              Attack
+            </button>
+            <button type="button" className="bg-blue-500 w-full h-8 text-white">
+              Defend
+            </button>
+          </div>
+        </Card>
+      </div>
     </div>
   );
 }
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -9,36 +9,38 @@ import {
 export default function Card(props) {
   const { image, name, intelligence, strength, life, children } = props;
   return (
-    <MTCard className="w-48 h-80 bg-black m-4 relatives">
-      <CardHeader floated={false} className="m-0">
-        <img className="" src={image} alt={name} />
-      </CardHeader>
-      <CardBody className="text-center p-0">
-        <Typography
-          variant="h4"
-          color="blue-gray "
-          className="mb-2  text-yellow-500"
-        >
-          {name}
-        </Typography>
-        <Typography className="grid grid-cols-2" textGradient>
-          <div className="flex justify-center items-center absolute top-1 left-2 shadow-lg bg-black p-0.05 rounded-xl border-red-400 border-solid border-2 ">
-            <img className="w-5 h-5" src="./icons/cerveau.png" alt="icon" />
-            <div className="text-white ">{intelligence}</div>
-          </div>
+    <div className="flex justify-center">
+      <MTCard className="w-48 h-80 bg-black m-4 relatives ">
+        <CardHeader floated={false} className="m-0">
+          <img className="" src={image} alt={name} />
+        </CardHeader>
+        <CardBody className="text-center p-0">
+          <Typography
+            variant="h4"
+            color="blue-gray "
+            className="mb-2  text-yellow-500"
+          >
+            {name}
+          </Typography>
+          <Typography className="grid grid-cols-2" textGradient>
+            <div className="flex justify-center items-center absolute top-1 left-2 shadow-lg bg-black p-0.05 rounded-xl border-red-400 border-solid border-2 ">
+              <img className="w-5 h-5" src="./icons/cerveau.png" alt="icon" />
+              <div className="text-white ">{intelligence}</div>
+            </div>
 
-          <div className="flex justify-center items-center absolute top-1 right-2 bg-black p-0.05 rounded-xl border-red-400 border-solid border-2">
-            <img className="w-5 h-5" src="./icons/sword.png" alt="icon" />
-            <div className="text-white ">{strength}</div>
-          </div>
-          <div className="flex justify-center items-center col-span-2">
-            <div className="bg-red-500 w-[100px] h-2" />
-            <div className="text-white ">{life}</div>
-          </div>
-        </Typography>
-      </CardBody>
-      {children}
-    </MTCard>
+            <div className="flex justify-center items-center absolute top-1 right-2 bg-black p-0.05 rounded-xl border-red-400 border-solid border-2">
+              <img className="w-5 h-5" src="./icons/sword.png" alt="icon" />
+              <div className="text-white ">{strength}</div>
+            </div>
+            <div className="flex justify-center items-center col-span-2">
+              <div className="bg-red-500 w-[100px] h-2" />
+              <div className="text-white ">{life}</div>
+            </div>
+          </Typography>
+        </CardBody>
+        {children}
+      </MTCard>
+    </div>
   );
 }
 
