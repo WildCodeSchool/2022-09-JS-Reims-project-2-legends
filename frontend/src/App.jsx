6b1ac8858049c88093d66688f056b67eecf3f403
iturnmyscreenon <benoit.galey@gmail.com>
2022-11-03 15:11:42 +0100

    Made homepage with tabs (Cards/Info/Rules) and one SampleCard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,11 @@
 import "./App.css";
-import React, { useState } from "react";
-import DraftPage from "./components/Draft/DraftPage";
+import React from "react";
 import HomePage from "./components/Homepage/Homepage";
-import CombatMode from "./components/Combat/CombatMode";
 
 export default function App() {
-  const [activePage, setActivePage] = useState("homepage");
-  const [draftRound, setDraftRound] = useState(1);
-  const [cardSelected, setCardSelected] = useState([]);
-  const [cardComputer, setCardComputer] = useState([]);
-  const play = () => {
-    setActivePage("draftPage");
-  };
   return (
     <div className="App">
-      {activePage === "homepage" && <HomePage play={play} />}
-      {activePage === "draftPage" && (
-        <DraftPage
-          draftRound={draftRound}
-          setDraftRound={setDraftRound}
-          cardSelected={cardSelected}
-          setCardSelected={setCardSelected}
-          cardComputer={cardComputer}
-          setCardComputer={setCardComputer}
-          setActivePage={setActivePage}
-        />
-      )}
-      {activePage === "combatpage" && (
-        <CombatMode
-          cardSelected={cardSelected}
-          setCardSelected={setCardSelected}
-          cardComputer={cardComputer}
-          setCardComputer={setCardComputer}
-        />
-      )}
+      <HomePage />
     </div>
   );
 }
