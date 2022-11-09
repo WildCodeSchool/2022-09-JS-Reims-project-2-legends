01dc5634872c47444f524a92bdc7ff54085b6190
Adnan Bakaev <adnanbakaev@MacBook-Air-Adnan.local>
2022-11-09 12:42:54 +0100

    changed card sample and switched to justify-start

--- frontend/src/components/Homepage/CardsTab.jsx
+++ frontend/src/components/Homepage/CardsTab.jsx
@@ -3,7 +3,7 @@ import SampleCard from "./SampleCard";
 
 function CardsTab() {
   return (
-    <section className="w-screen flex flex-col justify-between items-center min-h-96">
+    <section className="w-screen flex flex-col justify-start items-center min-h-96">
       <h3 className="w-screen text-center text-[#54EB75]">
         {"cards".toUpperCase()}
       </h3>
--- frontend/src/components/Homepage/SampleCard.jsx
+++ frontend/src/components/Homepage/SampleCard.jsx
@@ -6,12 +6,13 @@ import {
   Typography,
 } from "@material-tailwind/react";
 import sampleCardData from "./sampleCardData";
+import "./Homepage.css";
 
 export default function SampleCard() {
   return (
     <div className="flex justify-center box-content">
       <MTCard className="w-24 h-40 bg-black m-4 relatives border-2 border-black border-solid ">
-        <CardHeader floated={false} className="-m-0">
+        <CardHeader floated={false} className="-m-">
           <img
             className=""
             src={sampleCardData.image}
@@ -27,24 +28,72 @@ export default function SampleCard() {
             {sampleCardData.name}
           </Typography>
           <Typography className="grid grid-cols-2" textGradient>
-            <div className="flex justify-center items-center absolute top-[0%] -left-4 shadow-lg bg-black p-0.05 rounded-xl border-red-400 border-solid border-2 w-8 h-8">
-              <div className="text-white ">{sampleCardData.intelligence}</div>
+            <div className="hover-parent flex justify-center items-center absolute top-[0%] -left-5 shadow-lg bg-black p-0.05 rounded-xl border-red-400 border-solid border-2 w-10 h-8">
+              <section className=" show-on-hover">
+                <p>Intelligence </p>
+              </section>
+              <img
+                className="h-[20px] "
+                src="./icons/brainstorm.png"
+                alt="intelligence"
+              />
+              <div className="text-white ">81</div>
             </div>
-            <div className="flex justify-center items-center absolute top-[38%] -left-4 shadow-lg bg-black p-0.05 rounded-xl border-red-400 border-solid border-2 w-8 h-8">
-              <div className="text-white ">{sampleCardData.power}</div>
+            <div className="hover-parent flex justify-center items-center absolute top-[30%] -left-5 shadow-lg bg-black p-0.05 rounded-xl border-red-400 border-solid border-2 w-10 h-8">
+              <section className=" show-on-hover">
+                <p>Strength </p>
+              </section>
+              <img
+                className="h-[20px] "
+                src="./icons/magic.png"
+                alt="strength"
+              />
+              <div className="text-white ">40</div>
             </div>
-            <div className="flex justify-center items-center absolute top-[70%] -left-4 shadow-lg bg-black p-0.05 rounded-xl border-red-400 border-solid border-2 w-8 h-8">
-              <div className="text-white ">{sampleCardData.durability}</div>
+            <div className="hover-parent flex justify-center items-center absolute top-[58%] -left-5 shadow-lg bg-black p-0.05 rounded-xl border-red-400 border-solid border-2 w-10 h-8">
+              <section className=" show-on-hover">
+                <p>Durability </p>
+              </section>
+              <img
+                className="h-[20px] "
+                src="./icons/flash.png"
+                alt="durability"
+              />
+              <div className="text-white ">55</div>
             </div>
 
-            <div className="flex justify-center items-center absolute top-[0%] -right-4 bg-black p-0.05 rounded-xl border-red-400 border-solid border-2 h-8 w-8">
-              <div className="text-white ">{sampleCardData.strength}</div>
+            <div className="hover-parent flex justify-center items-center absolute top-[0%] -right-5 bg-black p-0.05 rounded-xl border-red-400 border-solid border-2 h-[33px] w-10">
+              <section className=" show-on-hover-right">
+                <p>Power </p>
+              </section>
+              <img
+                className="h-[20px] "
+                src="./icons/protest.png"
+                alt="power"
+              />
+              <div className="text-white ">63</div>
             </div>
-            <div className="flex justify-center items-center absolute top-[38%] -right-4 bg-black p-0.05 rounded-xl border-red-400 border-solid border-2 h-8 w-8">
-              <div className="text-white ">{sampleCardData.speed}</div>
+            <div className="hover-parent flex justify-center items-center absolute top-[30%] -right-5 bg-black p-0.05 rounded-xl border-red-400 border-solid border-2 h-[33px] w-10">
+              <section className=" show-on-hover-right">
+                <p>Combat </p>
+              </section>
+              <img
+                className="h-[20px] "
+                src="./icons/fighting.png"
+                alt="combat"
+              />
+              <div className="text-white ">90</div>
             </div>
-            <div className="flex justify-center items-center absolute top-[70%] -right-4 bg-black p-0.05 rounded-xl border-red-400 border-solid border-2 h-8 w-8">
-              <div className="text-white ">{sampleCardData.combat}</div>
+            <div className="hover-parent flex justify-center items-center absolute top-[58%] -right-5 bg-black p-0.05 rounded-xl border-red-400 border-solid border-2 h-[33px] w-10">
+              <section className=" show-on-hover-right">
+                <p>Speed </p>
+              </section>
+              <img
+                className="h-[20px] "
+                src="./icons/running.png"
+                alt="speed"
+              />
+              <div className="text-white ">29</div>
             </div>
           </Typography>
         </CardBody>
