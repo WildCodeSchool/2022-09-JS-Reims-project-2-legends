39176913033513c2ffb53c0047f67923aa406239
Adnan <adbak.dev@gmail.com>
2022-11-16 12:14:25 +0100

    removed Combat from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import React, { useState } from "react";
 import DraftPage from "./components/Draft/DraftPage";
 import HomePage from "./components/Homepage/Homepage";
-import CombatMode from "./components/Combat/CombatMode";
 
 export default function App() {
   const [activePage, setActivePage] = useState("homepage");
@@ -24,14 +23,6 @@ export default function App() {
           cardComputer={cardComputer}
           setCardComputer={setCardComputer}
           setActivePage={setActivePage}
-        />
-      )}
-      {activePage === "combatpage" && (
-        <CombatMode
-          cardSelected={cardSelected}
-          setCardSelected={setCardSelected}
-          cardComputer={cardComputer}
-          setCardComputer={setCardComputer}
         />
       )}
     </div>
