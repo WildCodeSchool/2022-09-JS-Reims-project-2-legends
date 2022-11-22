6540de5137a50788ef3e90e18287562ad602c268
Hugo <rodriguez.h@pm.me>
2022-11-22 10:32:16 +0100

    Adding the necessary imports/props for the operation of the play button and removing the unused ones

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,25 +4,38 @@ import DraftPage from "./components/Draft/DraftPage";
 import HomePage from "./components/Homepage/Homepage";
 
 export default function App() {
+  const [player1, setPlayer1] = useState("");
+  const [player2, setPlayer2] = useState("");
   const [activePage, setActivePage] = useState("homepage");
   const [draftRound, setDraftRound] = useState(1);
-  const [cardSelected, setCardSelected] = useState([]);
-  const [cardComputer, setCardComputer] = useState([]);
+  const [playerOneDeck, setPlayerOneDeck] = useState([]);
+  const [playerTwoDeck, setPlayerTwoDeck] = useState([]);
+
   const play = () => {
     setActivePage("draftPage");
   };
   return (
     <div className="App">
-      {activePage === "homepage" && <HomePage play={play} />}
+      {activePage === "homepage" && (
+        <HomePage
+          play={play}
+          player1={player1}
+          player2={player2}
+          setPlayer1={setPlayer1}
+          setPlayer2={setPlayer2}
+        />
+      )}
       {activePage === "draftPage" && (
         <DraftPage
           draftRound={draftRound}
           setDraftRound={setDraftRound}
-          cardSelected={cardSelected}
-          setCardSelected={setCardSelected}
-          cardComputer={cardComputer}
-          setCardComputer={setCardComputer}
+          playerOneDeck={playerOneDeck}
+          setPlayerOneDeck={setPlayerOneDeck}
+          playerTwoDeck={playerTwoDeck}
+          setPlayerTwoDeck={setPlayerTwoDeck}
           setActivePage={setActivePage}
+          player1={player1}
+          player2={player2}
         />
       )}
     </div>
