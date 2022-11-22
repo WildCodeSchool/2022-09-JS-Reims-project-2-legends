143304ab8fc7ece17e4ec360dd90b5d78ce1c24d
Romain Bronquard <romain.bronquard@gmail.com>
2022-11-22 15:10:27 +0100

    modif rules css

--- frontend/src/components/HomePage/RulesTab.jsx
+++ frontend/src/components/HomePage/RulesTab.jsx
@@ -2,24 +2,27 @@ import React from "react";
 
 function RulesTab() {
   return (
-    <section className="w-screen h-1/3 flex flex-col justify-between items-center">
-      <h3 className="w-screen text-center text-[#54EB75]">
-        {"règles".toUpperCase()}
-      </h3>
-      <p className="w-5/6 text-center text-white overflow-y-auto">
-        Début de partie : DRAFT PICK : Le 1er joueur à commencer le draft est
-        tiré au sort Le jeu propose 3 cartes, le 1er joueur pourra choisir la
-        PREMIERE carte, les deux restantes iront directement dans le deck du
-        2éme joueur. (Le draft continue alors jusqu'à ce que chaque joueur aient
-        6 cartes). La partie commence : Une stat est choisi aléatoirement par le
-        jeu et annoncé au début de la partie Chaque joueur choisit son héros
-        pour le placer sur le board, si la force des héros est équivalente une
-        autre stat est choisi par le jeu et celle ci sera prise en compte pour
-        la victoire la carte du joueur victorieux reste sur le board. La partie
-        se termine quand un des joueurs n’a plus de cartes. Le joueur peut
-        choisir quel stats supprimer à l'adversaire et vice-versa
-      </p>
-    </section>
+    <div className=" flex flex-col justify-between items-center ">
+      <h3 className="text-center text-[#54EB75]">{"règles".toUpperCase()}</h3>
+      <section className="w-5/6 h-4/5 flex overflow-auto flex-col justify-evenly bg-blue-gray-200 bg-opacity-10 rounded-xl">
+        <h4 className="pl-2 underline">DRAFT PICK :</h4>
+        <p className=" pl-2 text-white ">
+          Le joueur 1 commence la Draft, le jeu propose 3 cartes, le joueur
+          pourra choisir une carte, les deux autres iront directement dans le
+          deck du joueur adverse. La draft continue alors jusqu'à ce que chaque
+          joueur ait 6 cartes.
+        </p>
+        <h4 className="pl-2 underline">COMBAT :</h4>
+        <p className="pl-2 text-white ">
+          Une stat est choisie aléatoirement par le jeu et annoncé au début de
+          la partie. Chaque joueur choisit son héros pour le placer sur le
+          board, si la stat est équivalente pour les deux personnages aucun des
+          joueurs ne gagne de points autrement le personnage avec la stat dont
+          la valeur est la plus élevée donne 1 point au joueur. La partie se
+          termine quand les 2 joueurs n’ont plus de cartes.
+        </p>
+      </section>
+    </div>
   );
 }
 
